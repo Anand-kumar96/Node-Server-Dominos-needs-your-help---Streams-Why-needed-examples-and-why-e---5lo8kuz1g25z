51e4f565df3f1ddc1681ef6d3624e60a44d8d78c
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-05-21 01:45:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  const pathName=req.url;
+  if(pathName==='/welcome'){
+    res.writeHead(200, { 'content-Type': 'text/plain'})
+    res.end('Welcome to Dominos!')
+  }else if (pathName === '/contact') {
+    res.writeHead(200, { 'content-Type': 'application/json' })
+    res.end(
+      JSON.stringify({
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com',
+      })
+    )
+  }else{
+    res.writeHead(404, { 'content-Type': 'text/plain' })
+    res.end('404 Not found')
+  }
 }
-
+httpServer.listen(8081);
+console.log('server is running at port 8081')
 module.exports = httpServer;
